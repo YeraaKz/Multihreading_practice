6981fa8989a1eb8e0242015f1c743a295f4d2f12
YeraaKz <29525@iitu.edu.kz>
2023-07-30 20:04:04 +0600

    - Create Message class with a data field and accessor methods

---
+++ src/com/company/model/Message.java
@@ -0,0 +1,14 @@
+package com.company.model;
+
+public class Message {
+
+    private final String data;
+
+    public Message(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+}
